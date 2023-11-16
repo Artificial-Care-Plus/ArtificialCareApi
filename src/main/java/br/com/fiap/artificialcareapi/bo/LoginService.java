dc3db385e5df71c3c45e3bf4c70bffa3fa7dfd66
oleoz1n <leopity0@gmail.com>
2023-11-16 09:12:35 -0300

    Validações no LoginService

--- src/main/java/br/com/fiap/artificialcareapi/bo/LoginService.java
+++ src/main/java/br/com/fiap/artificialcareapi/bo/LoginService.java
@@ -2,12 +2,27 @@ package br.com.fiap.artificialcareapi.bo;
 
 import br.com.fiap.artificialcareapi.beans.Mensagem;
 import br.com.fiap.artificialcareapi.beans.Usuario;
+import br.com.fiap.artificialcareapi.dao.UsuarioDao;
 
 import java.sql.SQLException;
 import java.util.UUID;
 
 public class LoginService {
+    private static Mensagem validar(Usuario usuario) {
+        /* As outras validações serão feitas no front-end */
+        if (!usuario.getEmail().contains("@")) {
+            return new Mensagem("Email inválido", false);
+        }
+
+        return new Mensagem("Usuário válido", true);
+    }
+
+
     public static Mensagem login(Usuario usuario) throws SQLException {
+        var validacao = validar(usuario);
+        if (!validacao.isSucesso()) {
+            return validacao;
+        }
         var usuarioDados = UsuarioService.find(usuario.getEmail());
         if (usuarioDados == null) {
             return new Mensagem("Email não existe", false);
